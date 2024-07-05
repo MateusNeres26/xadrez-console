57efa10de805c92f11cb52db94f4bde0f0e10421
Mateus Neres <mponeres3@gmail.com>
2024-07-05 19:41:37 -0300

    Imprimindo tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,10 +1,13 @@
 ﻿using tabuleiro;
+using xadrez_console;
 
 internal class Program {
     private static void Main(string[] args) {
         Posicao P;
 
         Tabuleiro tab = new Tabuleiro(8, 8);
+
+        Tela.imprimirTabuleiro(tab);
 
         Console.ReadLine();
     }
